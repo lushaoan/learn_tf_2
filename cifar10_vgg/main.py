886634edac13f46d7398edac6154fcbe1eb54d65
lushaoan <422930673@qq.com>
2019-09-01 13:32:59 +0800

    add vgg

---
+++ cifar10_vgg/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+# -*- coding:utf-8 -*-
+__author__ = 'Lu ShaoAn'
+__version__ = '1.0'
+__date__ = '2019.09.01'
+__copyright__ = 'Copyright 2019, PI'
+
+
+import tensorflow as tf
